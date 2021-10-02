c8754a1e4c499bc5c71337e012ebec9f5ca589e9
Jerone Poh <1902606@sit.singaporetech.edu.sg>
2021-10-03 00:57:18 +0800

    test

--- ICT3101_Calculator/Calculator.cs
+++ ICT3101_Calculator/Calculator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 /*ICT3101: Lab 3 changes, now can run 2 solution files. (Latest on 3 October 2021)
-Prepared by: Jerone Poh Lab Group P1
+Prepared by: Jerone Poh Lab Group P1 2021/22
 */
 
 namespace ICT3101_Calculator
